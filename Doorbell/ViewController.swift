aae4a68e0a08b32bf53c25c8de94a6914b2e3d48
Josh McMillan <josh@joshmcmillan.co.uk>
2014-11-17 14:20:12 +0000

    Initial Commit

---
+++ Doorbell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Doorbell
+//
+//  Created by Josh McMillan on 17/11/2014.
+//  Copyright (c) 2014 Lost My Name. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
