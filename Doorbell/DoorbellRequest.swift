737d6a3a35313720f9fffa135d52fbe7379921f2
Josh McMillan <josh@joshmcmillan.co.uk>
2014-11-20 17:50:44 +0000

    Flesh out design/functionality

---
+++ Doorbell/DoorbellRequest.swift
@@ -0,0 +1,26 @@
+//
+//  DoorbellRequest.swift
+//  Doorbell
+//
+//  Created by Josh McMillan on 17/11/2014.
+//  Copyright (c) 2014 Lost My Name. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class DoorbellRequest: NSObject {
+    class func send() {
+        Alamofire.request(.POST, "https://lostmydoorbell.herokuapp.com/", parameters: [
+            "token": self.slackToken(),
+            "user_name": UIDevice.currentDevice().name
+        ])
+    }
+    
+    class func slackToken() -> AnyObject {
+        let path = NSBundle.mainBundle().pathForResource("settings", ofType: "plist")
+        let settings = NSDictionary(contentsOfFile: path!)!
+        
+        return settings["SlackToken"]!
+    }
+}
--- Doorbell/ViewController.swift
+++ Doorbell/ViewController.swift
@@ -9,17 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var opener: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.setBackground()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func openDoor() {
+        DoorbellRequest.send()
+    }
+    
+    func setBackground() {
+        var sourceImage = UIImage(named: "background.jpg")
+        let imageSize = sourceImage?.size
+        let width = imageSize?.width
+        let height = imageSize?.height
+        let targetSize = self.view.frame.size
+        let targetWidth = targetSize.width
+        let targetHeight = targetSize.height
+        var scaleFactor:CGFloat = 0.0
+        var scaledWidth = targetWidth
+        var scaledHeight = targetHeight
+        var thumbnailPoint = CGPointMake(0.0, 0.0)
+        
+        if !CGSizeEqualToSize(imageSize!, targetSize) {
+            let widthFactor  = targetWidth / width!
+            let heightFactor = targetHeight / height!
+            
+            scaleFactor = widthFactor > heightFactor ? widthFactor : heightFactor
+            
+            scaledWidth = width! * scaleFactor
+            scaledHeight = height! * scaleFactor
+            
+            if widthFactor > heightFactor {
+                thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5
+            }
+            else if widthFactor < heightFactor {
+                thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5
+            }
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(targetSize, false, 0.0)
+        var thumbnailRect = CGRectZero
+        thumbnailRect.origin = thumbnailPoint
+        thumbnailRect.size.width = scaledWidth
+        thumbnailRect.size.height = scaledHeight
+        
+        sourceImage?.drawInRect(thumbnailRect)
+        
+        var newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        self.view.backgroundColor = UIColor(patternImage: newImage)
+    }
 
 }
 
