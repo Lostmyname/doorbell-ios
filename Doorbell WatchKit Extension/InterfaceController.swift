92c5b841858e7bb2fb36cee05441727518c197c9
Simon Fry <simonf@lostmy.name>
2015-05-04 15:37:44 +0100

    Super basic watch app.

---
+++ Doorbell WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,34 @@
+//
+//  InterfaceController.swift
+//  Doorbell WatchKit Extension
+//
+//  Created by Simon Fry on 04/05/2015.
+//  Copyright (c) 2015 Lost My Name. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    @IBAction func openDoor() {
+        DoorbellRequest.send()
+    }
+}
