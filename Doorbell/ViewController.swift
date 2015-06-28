e622941ae3183ba5440f09d5abd83f00aab47512
Simon Fry <simonf@lostmy.name>
2015-06-28 12:11:06 +0100

    Start of iBeacon work.

--- Doorbell/ViewController.swift
+++ Doorbell/ViewController.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet var opener: UIButton!
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.setBackground()
+        self.locationManager.delegate = self
+        self.locationManager.requestAlwaysAuthorization()
     }
 
     override func didReceiveMemoryWarning() {
@@ -69,6 +73,32 @@ class ViewController: UIViewController {
         UIGraphicsEndImageContext()
         
         self.view.backgroundColor = UIColor(patternImage: newImage)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        self.setupIBeaconMonitoring()
+    }
+    
+    func setupIBeaconMonitoring() {
+        println("Start setting up monitoring")
+        if (CLLocationManager.isMonitoringAvailableForClass(CLBeaconRegion)) {
+            println("Can monitor regions")
+        }
+        var proximityUUID = NSUUID(UUIDString: "43BD5014-5743-463A-9B9F-A47ECA30DF51")
+        var beaconRegion = CLBeaconRegion(proximityUUID: proximityUUID, identifier: "DoorBeacon")
+        beaconRegion.notifyEntryStateOnDisplay = false
+        beaconRegion.notifyOnEntry = true
+        beaconRegion.notifyOnExit = false
+        self.locationManager.startMonitoringForRegion(beaconRegion)
+        println("Finish setting up monitoring")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didDetermineState state: CLRegionState, forRegion region: CLRegion!) {
+        println("didDetermineState")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+        println("didEnterRegion")
     }
 
 }
